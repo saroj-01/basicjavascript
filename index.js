2f56a6b4ac1d423a52fe9b0b20ddd230d584181e
saroj-01 <shah.saroz748@gmail.com>
2023-05-30 03:01:33 -0700

    add basicjavascript

---
+++ index.js
@@ -0,0 +1,8 @@
+const length = prompt("Enter length");
+const width = prompt("Enter width");
+const area = length * width;
+console.log(area);
+
+alert("The area of recgtangle is :" + area);
+
+document.write("The area of ractangle is: " + area);
