9fae609eec7771db5abdbd245410d692aae4a204
saroj-01 <shah.saroz748@gmail.com>
2023-06-12 03:41:11 -0700

    project

--- index.js
+++ index.js
@@ -370,26 +370,130 @@
 // console.log(result);
 // console.log(arr);
 
-const Input = document.querySelector("input");
-const AddBtn = document.querySelector("#add");
-const Btnlogout = document.querySelector("#clear");
+// const Input = document.querySelector("input");
+// const AddBtn = document.querySelector("#add");
+// const Btnlogout = document.querySelector("#clear");
 
-const AddWebStorage = () => {
-  localStorage.setItem(
-    "key",
-    JSON.stringify({
-      name: "ram",
-      age: 34,
-      gender: "male",
-      Ismarried: false,
-    })
+// const AddWebStorage = () => {
+//   localStorage.setItem(
+//     "key",
+//     JSON.stringify({
+//       name: "ram",
+//       age: 34,
+//       gender: "male",
+//       Ismarried: false,
+//     })
+//   );
+// };
+// AddBtn.addEventListener("click", AddWebStorage);
+// Btnlogout.addEventListener("click", () => {
+//   localStorage.clear();
+// });
+// alert(localStorage.getItem("key"));
+// if (localStorage.getItem("key")) {
+//   console.log(JSON.parse(localStorage.getItem("key")));
+// }
+
+// Statics property and method
+
+// class Auth {
+//   static dbEmail = "eamil@gmail.com";
+//   static dbPassword = "Password";
+//   static dbToken = "njsanjns";
+// }
+// console.log(Auth.dbEmail);
+
+// class Person {
+//   constructor(name, age, income) {
+//     this.name = name;
+//     this.age = age;
+//     this.income = income;
+//   }
+//   static compareByFilter(value, index) {
+//     return value.income > 1000;
+//   }
+// }
+
+// const user1 = new Person("Ram", 33, 45000);
+// const user2 = new Person("Laxman", 67, 7889);
+// const user3 = new Person("sam", 78, 8790);
+
+// const users = [user1, user2, user3];
+// const result = users.filter(Person.compareByFilter);
+// console.log(result);
+
+// setTimeout(() => {
+//   alert("Hello World");
+// }, 3000);
+// console.log("first");
+// console.log("second");
+// console.log("third");
+
+// function add(a, b, callback) {
+//   const result = a + b;
+//   callback(result);
+// }
+
+// function displaySum(ans) {
+//   console.log(`the sum of two nember:${ans}`);
+// }
+
+// add(4, 3, displaySum);
+
+// const ans = add(4, 3);
+// console.log(ans);
+
+// let myArr = [7, 8, 5, 6, 9, 0];
+// function calculation(arr = [], cb) {
+//   let ans = [];
+
+//   for (let i = 0; i < arr.length; i++) {
+//     ans.push(cb(arr[i]));
+//   }
+//   console.log(ans);
+// }
+
+// // function multiply(a) {
+// //   return a * 10;
+// // }
+// // function divide(a) {
+// //   return a / 10;
+// // }
+// // wrap above into one line code
+// calculation(myArr, (a) => a * 10);
+// calculation(myArr, (a) => a / 10);
+// calculation(myArr, (a) => a + 5);
+// calculation(myArr, (a) => a - 5);
+
+let personList = [];
+const fetchedData = (callBack) => {
+  setTimeout(
+    (callBack) => {
+      personList.push(
+        { id: 1, name: "Ram", age: 32 },
+        { id: 2, name: "hari", age: 45 },
+        { id: 3, name: "gita", age: 43 },
+        { id: 4, name: "sita", age: 12 }
+      );
+
+      callBack();
+      console.log(personList);
+    },
+    4000,
+    callBack
   );
 };
-AddBtn.addEventListener("click", AddWebStorage);
-Btnlogout.addEventListener("click", () => {
-  localStorage.clear();
-});
-alert(localStorage.getItem("key"));
-if (localStorage.getItem("key")) {
-  console.log(JSON.parse(localStorage.getItem("key")));
-}
+// console.log(personList);
+// fetchedData(dispalyName);
+// console.log(personList);
+const dispalyName = () => {
+  setTimeout(() => {
+    for (let i = 0; i < personList.length; i++) {
+      const p = document.createElement("p");
+      p.innerHTML = personList[i].name;
+      document.body.append(p);
+    }
+  }, 1000);
+};
+
+fetchedData(dispalyName);
